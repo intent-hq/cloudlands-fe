1294325368ebe3118c6a9b33c854c028d968fc44
Amelia Wattenberger <wattenberger@gmail.com>
2026-09-06 21:19:21 +0000

    chore(icons): drop unused faArrowRotateLeft export

--- src/lib/icons/phosphor-icons.ts
+++ src/lib/icons/phosphor-icons.ts
@@ -416,7 +416,6 @@ export const faArrowLeft = icon('arrow-left');
 export const faArrowPointer = icon('arrow-pointer');
 export const faArrowRight = icon('arrow-right');
 export const faArrowRightArrowLeft = icon('arrow-right-arrow-left');
-export const faArrowRotateLeft = icon('arrow-rotate-left');
 export const faArrowRotateRight = icon('arrow-rotate-right');
 export const faArrowUp = icon('arrow-up');
 export const faArrowUpFromBracket = icon('arrow-up-from-bracket');
