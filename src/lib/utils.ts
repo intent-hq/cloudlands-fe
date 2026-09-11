db631ba463c76c5c35fe603505d6c133a9681bd4
Amelia Wattenberger <wattenberger@gmail.com>
2026-09-11 01:49:30 +0000

    fix(ui): merge semantic typography roles

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,4 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+export { cn } from './utils/cn';
 
 // Type utilities for component props
 export type WithElementRef<T, E = HTMLElement> = T & {
